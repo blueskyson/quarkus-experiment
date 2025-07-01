d08d4f693adcd0e26120a4af793871637e7dd4c8
Jack Lin <blueskyson1401@gmail.com>
2025-07-01 23:23:11 +0800

    Add @Nonbinding to fix annotation

--- src/main/java/org/acme/auth/RequirePermission.java
+++ src/main/java/org/acme/auth/RequirePermission.java
@@ -1,17 +1,16 @@
 package org.acme.auth;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import jakarta.interceptor.InterceptorBinding;
+import jakarta.enterprise.util.Nonbinding;
 
 @InterceptorBinding
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
-@Inherited
 public @interface RequirePermission {
-    String[] value(); // The permissions required to access the resource
+    @Nonbinding String[] value(); // The permissions required to access the resource
 }
